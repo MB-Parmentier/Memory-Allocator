e3b300838df9a53d639e92e1935fe457488c7a03
MB-Parmentier <103027428+MB-Parmentier@users.noreply.github.com>
2024-11-10 03:00:24 +0100

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+#![no_std]
+#![no_main]
+/// Ce code implante un allocateur dynamique sans la libraire standard
+/// On utilise le trait Global Allocator
+
+
+//fn main() {
+//    println!("Hello, world!");
+//}
