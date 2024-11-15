8a7b358ca8c3eaaad7a2c008f05b65bc58ee7665
MB-Parmentier <103027428+MB-Parmentier@users.noreply.github.com>
2024-11-15 20:37:06 +0100

    Syscall brk + syscall write

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,42 @@
 #![no_std]
 #![no_main]
+use core::panic::PanicInfo;
 /// Ce code implante un allocateur dynamique sans la libraire standard
 /// On utilise le trait Global Allocator
 
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    let result: i64;
 
-//fn main() {
-//    println!("Hello, world!");
-//}
+    unsafe {
+        core::arch::asm!(
+            "mov rax, 12",      // Syscall brk()
+            "mov rdi, 0",
+            "syscall",
+            "mov {}, rax",       // Stocker le résultat de rax dans la variable result
+            out(reg) result,     // Associer le registre de sortie à result
+            options(nostack, preserves_flags)
+        );
+    
+
+    // result contient l'adresse de brk
+
+        // Utilise la valeur de result dans un autre appel système
+        core::arch::asm!(
+            "mov rax, 1",        // Syscall pour write (stdout)
+            "mov rdi, 1",        // Descripteur de fichier pour stdout
+            "mov rsi, {0}",      // Adresse du buffer (result)
+            "mov rdx, 8",        // Nombre d'octets à écrire (8 pour i64)
+            "syscall",
+            in(reg) &result,     // Passe l'adresse de "result" à rsi
+            options(nostack, preserves_flags)
+        );
+    }
+
+    loop {}
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
